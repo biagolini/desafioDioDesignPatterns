387ab124d27d5b87aa64bbc0f90142d252b74eae
Carlos Biagolini-Jr <c.biagolini@gmail.com>
2022-03-06 13:23:03 -0300

    Parte 01 Menu de navegação

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import br.com.operador.Menu;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("MAIN WORKS");
+        Menu menu = new Menu();
+        menu.menu();
     }
 }
---
+++ src/br/com/operador/Menu.java
@@ -0,0 +1,69 @@
+package br.com.operador;
+
+import java.util.Scanner;
+
+public class Menu {
+    Scanner scanner = new Scanner(System.in);
+
+    private void singleton() {
+        System.out.printf("Exemplo de uso do padrão Singleton...\n");
+    }
+
+    private void strategy() {
+        System.out.printf("Exemplo de uso do padrão Strategy...\n");
+    }
+
+    private void facade() {
+        System.out.printf("Exemplo de uso do padrão Facade...\n");
+    }
+
+    public void menu(){
+        Integer opcao = 0;
+        do{
+            System.out.println(formatRow("!---------!-------------------------------------#"));
+            System.out.println(formatRow("|  Opção  | Descrição                           |"));
+            System.out.println(formatRow("$---------*-------------------------------------("));
+            System.out.println(formatRow("|    1    | Teste o uso do padrão Singleton     |"));
+            System.out.println(formatRow("|    2    | Teste o uso do padrão Strategy      |"));
+            System.out.println(formatRow("|    3    | Teste o uso do padrão Facade        |"));
+            System.out.println(formatRow("|    0    | Sair                                |"));
+            System.out.println(formatRow(")---------+-------------------------------------^"));
+            System.out.println("\033[3mDigite o número correspondente a opção desejada:\033[0m");
+
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcao) {
+                case 1: singleton(); break;
+                case 2: strategy(); break;
+                case 3: facade(); break;
+                case 0:
+                    System.out.println(formatRow("-------------------------------------------------"));
+                    System.out.println(formatRow("\033[3m Obrigado por utilizar nosso software, até logo \033[0m"));
+                    break;
+                default:System.out.printf("Opção inválida;\n");
+            }
+            System.out.printf("\033[0;1m" +"\n\n\t\t\t\t\t\033[3mPressione ENTER para prosseguir\033[0m");
+            scanner.nextLine();
+
+        } while (opcao!=0);
+    }
+
+    public static String formatRow(String str) {
+        return str
+                .replace('|', '\u2502')
+                .replace('!', '\u250c')
+                .replace('@', '\u252c')
+                .replace('#', '\u2510')
+                .replace('$', '\u251c')
+                .replace('*', '\u253c')
+                .replace('(', '\u2524')
+                .replace(')', '\u2514')
+                .replace('+', '\u2534')
+                .replace('^', '\u2518')
+                .replace('-', '\u2500');
+    }
+
+
+
+}
